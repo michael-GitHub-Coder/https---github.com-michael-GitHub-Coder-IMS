f325b82bff1e64ccfa0081d104d77d986d391115
Michael <motlokwa.thulare@gmail.com>
2025-02-05 12:16:01 +0200

    ed

--- Backend/Controllers/group.controller.js
+++ Backend/Controllers/group.controller.js
@@ -25,17 +25,5 @@ export const addToGroup = async (req, res) =>{
     if(!groupId || !userId){
         return res.status(400).json({message:"All fields are required"});
     }
-    try {
-        const group = await Group.findById(groupId);
-        if(!group){
-            return res.status(404).json({message:"Group not found"});
-        }
-        group.members.push(userId);
-        await group.save();
-        res.status(200).json({message:"User added to group successfully",group});
-
-    } catch (error) {
-        console.log("addToGroup error", error);
-        res.status(500).json({message:error.message})
-    }
+   
 }
