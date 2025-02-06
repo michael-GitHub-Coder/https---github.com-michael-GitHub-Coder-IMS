470656b71ba69f885d46ab958b8040163f6e5452
Michael <motlokwa.thulare@gmail.com>
2025-02-06 12:43:35 +0200

    gehth

--- Backend/Controllers/group.controller.js
+++ Backend/Controllers/group.controller.js
@@ -1,4 +1,5 @@
 import {Group} from '../Models/group.model.js';
+import {User} from '../Models/User.model.js';
 
 export const addGroup = async (req, res) =>{
     
@@ -31,17 +32,45 @@ export const getAllGroups = async (req,res) =>{
         res.status(500).json({message:error.message});
     }
 }
+
+
 export const addToGroup = async (req, res) =>{
     
     const {regionId, supervisorId} = req.body;
-    if(!groupId || !supervisorId){
+    const {id} = req.params;
+
+    
+
+    if(!regionId || !supervisorId){
         return res.status(400).json({message:"All fields are required"});
     }
 
     try {
         
+        const user = await User.findById(req.userId).select("-password");
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        if(user.role !== "Admin"){
+            return res.status(401).json({message:"You are not authorized to perform this action"});
+        }
+
+        const group = await Group.findById(id);
+        if(!group){
+            return res.status(404).json({message:"Group not found"});
+        }
+      
+        group.name = group.name;
+        group.regionId = regionId;
+        group.supervisorId = supervisorId;
+
+        await group.save();
+        res.status(201).json({message:"Supervisor added to group:",groupName:group.name,group});
+
     } catch (error) {
         
+        console.log("addToGroup error", error);
+        res.status(500).json({message:error.message})
     }
    
 }
