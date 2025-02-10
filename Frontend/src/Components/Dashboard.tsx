4555dcdcfbf087450f65730c443685b35a7ce4e6
Michael <motlokwa.thulare@gmail.com>
2025-02-10 15:50:56 +0200

    sfdfhku

--- Frontend/src/Components/Dashboard.tsx
+++ Frontend/src/Components/Dashboard.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
-import { FaArrowRight } from "react-icons/fa";
-import { FiMenu, FiHome, FiSettings, FiUser } from "react-icons/fi";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { FiHome, FiSettings, FiUser } from "react-icons/fi";
 
 const Sidebar = ({ isOpen }: { isOpen: boolean }) => {
   return (
-    <div className={`bg-indigo-500 rounded-md mt-5 text-white p-5 fixed top-0 left-0 transition-transform ${
-            isOpen ? "w-60" : "w-16"
-        } h-[calc(100vh-50px)]`}
-        >
+    <div
+      className={`bg-indigo-500 rounded-md mt-5 text-white p-5 fixed top-0 left-0 transition-all duration-300 ${
+        isOpen ? "w-60" : "w-16"
+      } h-[calc(100vh-50px)]`}
+    >
       <div className="flex flex-col space-y-6">
         <div className="flex items-center space-x-3">
           <FiHome size={24} />
@@ -31,16 +32,19 @@ const Dashboard = () => {
 
   return (
     <div className="flex h-screen">
-        <div > 
-            <div className="flex relative">
-                <Sidebar isOpen={sidebarOpen} />
-            </div>
-            <div>
-                <button className="text-white rounded-r-full bg-indigo-500 pr-2 py-4 absolute left-13 top-7" onClick={() => setSidebarOpen(!sidebarOpen)}>
-                    <FaArrowRight size={24} />
-                </button>
-            </div>  
-        </div>
+   
+      <div className="relative">
+        
+        <Sidebar isOpen={sidebarOpen} />
+
+        <button
+          className="text-white rounded-r-full bg-indigo-500 pr-3 pl-2 py-4 absolute top-7 transition-all duration-300"
+          style={{ left: sidebarOpen ? "15rem" : "3.5rem" }} 
+          onClick={() => setSidebarOpen(!sidebarOpen)}
+        >
+          {sidebarOpen ? <FaArrowLeft size={24} /> : <FaArrowRight size={24} />}
+        </button>
+      </div>
     </div>
   );
 };
