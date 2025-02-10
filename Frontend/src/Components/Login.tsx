ec11cfbaf3cd6f7d49fcee1ee03292922f36db1f
Michael <motlokwa.thulare@gmail.com>
2025-02-10 14:32:18 +0200

    efef

--- Frontend/src/Components/Login.tsx
+++ Frontend/src/Components/Login.tsx
@@ -7,18 +7,22 @@ const Login = () => {
           <div className="flex flex-col">
               <p className="text-5xl font-bold mt-10">Welcome Back!</p>
               <p className="text-gray-400 -mt-1.5 font-bold ">Please enter login details below</p>
-              <input type="text" placeholder="Enter the email" className="font-bold px-2 py-3 rounded-md text-gray-500  bg-gray-300 w-96 mt-5"/>
-              <input type="text" placeholder="Enter the Password" className="font-bold px-2 py-3 rounded-md text-gray-500 bg-gray-300 w-96 mt-5"/>
-              <p className="flex py-3 justify-end text-black font-bold">Forgot Password?</p>
-              <button className="font-bold px-2 py-3 rounded-md bg-gray-300 w-96  text-gray-500 ">Sign in</button>
-              <div className="flex text-gray-500">
+              <input type="text" placeholder="Enter the email" className="font-bold px-2 py-3 rounded-md text-gray-500  bg-gray-300 w-96 mt-7"/>
+              <input type="text" placeholder="Enter the Password" className="font-bold px-2 py-3 rounded-md text-gray-500 bg-gray-300 w-96 mt-7"/>
+              <p className="flex py-3 justify-end text-black font-bold cursor-pointer">Forgot Password?</p>
+              <button className="font-bold px-2 py-3 rounded-md bg-gray-300 w-96  text-gray-500 hover:text-white cursor-pointer hover:bg-gray-500">Sign in</button>
+              <div className="flex text-gray-500 py-4">
                 <p className="border-b-2 border-gray-500 w-[45%] mb-2"></p>
                 <p className="text-gray-400 mx-2 mt-2">OR</p>
                 <p className="border-b-2 border-gray-500 w-[45%] mb-2"></p>
               </div>
+              <div className="flex justify-center text-gray-500 gap-4 font-semibold py-4">
+                <p>Don’t have an account? </p>
+                <button className="text-indigo-500 cursor-pointer">Sign up</button>
+              </div>
           </div>
           <div className="bg-indigo-500 rounded-md px-2 py-1 w-96 opacity-80 ">
-              rgr
+              
           </div>
         </div>
     </div>
