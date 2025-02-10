b68c4fbf884d90f651baacc15c238f06371021c6
Michael <motlokwa.thulare@gmail.com>
2025-02-10 14:21:44 +0200

    erthj

--- Frontend/src/Components/Login.tsx
+++ Frontend/src/Components/Login.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 const Login = () => {
   return (
     <div className="flex justify-center items-center h-screen ">
-        <div className="bg-white px-2 py-4 flex flex-row gap-5 mmin-w-8xl">
-          <div>
-              <p className="text-5xl font-bold ">Welcome Back!</p>
-              <p className="text-gray-400 -mt-1.5 font-bold">Please enter login details below</p>
-              <input type="text" placeholder="Username" className="border-b-2 border-gray-300 w-96 mt-5"/>
+        <div className="bg-white px-5 py-4 flex flex-row gap-5 mmin-w-8xl">
+          <div className="flex flex-col">
+              <p className="text-5xl font-bold mt-10">Welcome Back!</p>
+              <p className="text-gray-400 -mt-1.5 font-bold ">Please enter login details below</p>
+              <input type="text" placeholder="Enter the email" className="font-bold px-2 py-3 rounded-md text-gray-500  bg-gray-300 w-96 mt-5"/>
+              <input type="text" placeholder="Enter the Password" className="font-bold px-2 py-3 rounded-md text-gray-500 bg-gray-300 w-96 mt-5"/>
+              <p className="flex py-3 justify-end text-black font-bold">Forgot Password?</p>
+              <button className="font-bold px-2 py-3 rounded-md bg-gray-300 w-96  text-gray-500 ">Sign in</button>
           </div>
           <div className="bg-indigo-500 rounded-md px-2 py-1 w-96 opacity-80 ">
               rgr
