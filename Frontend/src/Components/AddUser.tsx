9cfc2112588fb37dac9311cf56332b35fd1799b9
Michael <motlokwa.thulare@gmail.com>
2025-02-12 11:51:21 +0200

    dfsgh

--- Frontend/src/Components/AddUser.tsx
+++ Frontend/src/Components/AddUser.tsx
@@ -4,7 +4,7 @@ const AddUser = () => {
   return (
     <div>
         <h1 className="text-center font-bold text-2xl -mt-10">Add new user</h1>
-        <div className="flex flex-col justify-center px-5 min-w-6xl w-auto ml-10 mr-17 mt-5">
+        <div className="flex flex-col justify-center px-50 min-w-6xl w-auto ml-10 mr-17 mt-5">
             <form>
                 <div className="grid gap-6 mb-6  my-5">  
                     <input 
@@ -47,9 +47,9 @@ const AddUser = () => {
                             <option value="closed">Closed</option>
                         </select>
                     </div>
-
+                    <button type="submit" className="cursor-pointer w-full text-white bg-indigo-500 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Add User</button>
                 </div>
-                <button type="submit" className="cursor-pointer text-white bg-indigo-500 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Add User</button>
+                
             
             </form>
 
