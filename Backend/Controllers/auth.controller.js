31d280d2aab87b9c64b3fabb6ca401dfb75f0edc
Michael <motlokwa.thulare@gmail.com>
2025-03-10 09:29:18 +0200

    Is ve

--- Backend/Controllers/auth.controller.js
+++ Backend/Controllers/auth.controller.js
@@ -68,9 +68,9 @@ export const login = async (req, res) => {
 		if (!isPasswordValid) {
 			return res.status(BAD_REQUEST).json({ success: false, message: "Invalid credentials" });
 		}
-	    // if(!user.isVerified){
-		// 	return res.status(400).json({success:false, message:"Email not verified"});
-		// }
+	    if(!user.isVerified){
+			return res.status(400).json({success:false, message:"Email not verified"});
+		}
 
 		generateTokenAndsetCookie(res, user._id);
 
